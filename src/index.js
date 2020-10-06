87c77eba3de0d660dcaedba053b81f8b69bab86e
Artoym <44746890+Flashbuck@users.noreply.github.com>
2020-10-06 13:05:49 +0300

    feat: add script task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  let newstr = str.slice(),
+      length = 0,
+      Brack = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      Brack.push(bracketsConfig[i].join(''));
+  }
+  while (newstr.length !== 0) {
+      for (let i = 0; i < newstr.length; i++) {
+          if (Brack.includes(newstr[i] + newstr[i + 1])) {
+              newstr.splice(i, 2).join('');
+          }
+      }
+      if(length > str.length){
+        return false;
+      }
+      length+=1;
+  }
+  return true;
 }
